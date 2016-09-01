5d983166e9769049add53d5e655f88a2d857cbd7
YourtionGuo <yourtion@gmail.com>
2016-09-01 10:53:42 +0800

    update react rules

--- react.js
+++ react.js
@@ -14,8 +14,22 @@ module.exports = {
     'react',
   ],
   rules: {
-    // react
+    // React Config
+    // Enforce boolean attributes notation in JSX (fixable)
+    'react/jsx-boolean-value': 'warn', 
+    // Validate closing bracket location in JSX (fixable)
+    'react/jsx-closing-bracket-location': 'warn',
+    // Enforce or disallow spaces inside of curly braces in JSX attributes (fixable)
+    'react/jsx-curly-spacing': 'warn',
+    // Enforce or disallow spaces around equal signs in JSX attributes (fixable)
+    'react/jsx-equals-spacing': 'warn',
+    // Validate spacing before closing bracket in JSX (fixable)
+    'react/jsx-space-before-closing': 'warn',
+    // Prevent React to be incorrectly marked as unused
     'react/jsx-uses-react': 'error',
+    // Prevent variables used in JSX to be incorrectly marked as unused
     'react/jsx-uses-vars': 'error',
+    // Prevent missing parentheses around multilines JSX (fixable)
+    'react/jsx-wrap-multilines': 'warn',
   },
 };
