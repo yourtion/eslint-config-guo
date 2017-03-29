39121f882871b7c71d50ee76c8e297794cd45491
YourtionGuo <yourtion@gmail.com>
2017-03-29 10:05:04 +0800

    add es5

---
+++ es5.js
@@ -0,0 +1,14 @@
+const path = require('path');
+module.exports = {
+  'extends': path.resolve(__dirname, './index.js'),
+  'ecmaFeatures': {
+    'modules': false,
+    'experimentalObjectRestSpread': false,
+  },
+  'env': {
+    'es6': false,
+  },
+  'rules': {
+    'no-var': 'off',
+  },
+};
