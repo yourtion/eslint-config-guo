7d6e40954dade4a738678f54063655e93ad20613
YourtionGuo <yourtion@gmail.com>
2017-07-13 10:36:05 +0800

    update es5

--- es5.js
+++ es5.js
@@ -1,14 +1,14 @@
 const path = require('path');
 module.exports = {
-  'extends': path.resolve(__dirname, './index.js'),
-  'ecmaFeatures': {
-    'modules': false,
-    'experimentalObjectRestSpread': false,
+  extends: path.resolve(__dirname, './index.js'),
+  ecmaFeatures: {
+    modules: false,
+    experimentalObjectRestSpread: false,
   },
-  'env': {
-    'es6': false,
+  env: {
+    es6: false,
   },
-  'rules': {
+  rules: {
     'no-var': 'off',
   },
 };
