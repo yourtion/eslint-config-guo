5be6775bf5cbb7c3989f944b3d66d83768307146
YourtionGuo <yourtion@gmail.com>
2019-06-16 16:51:37 +0800

    fix: rm experimentalObjectRestSpread

--- es5.js
+++ es5.js
@@ -1,9 +1,11 @@
 const path = require('path');
 module.exports = {
   extends: path.resolve(__dirname, './index.js'),
+  parserOptions: {
+    ecmaVersion: 5,
+  },
   ecmaFeatures: {
     modules: false,
-    experimentalObjectRestSpread: false,
   },
   env: {
     es6: false,
--- vue.js
+++ vue.js
@@ -4,7 +4,6 @@ module.exports = {
   parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
-      experimentalObjectRestSpread: true,
       jsx: true,
     },
     sourceType: 'module',
